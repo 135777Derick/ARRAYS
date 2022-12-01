61513ba333f8ef08617eba86fcc92478c2369c78
135777Derick <117374653+135777Derick@users.noreply.github.com>
2022-12-01 16:49:42 +0300

    Update accessArray.cpp

--- accessArray.cpp
+++ accessArray.cpp
@@ -18,3 +18,22 @@ int main() {
 
   return 0;
 }
+
+
+
+
+// summing all elements of an array;
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int nums[] = {10,20,30,40,50,60,70,80,90,100};
+  int sum = 0;
+  for (int i = 0; i < 10; i++){
+      sum = sum + nums [i];
+  }
+
+  cout <<"The sum of array element is: \n"<< sum  <<endl;
+  return 0;
+}
